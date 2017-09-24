b15d0639a2f5752019c385282e09751a4cac6a35
BernhardB <schluchti@gmail.com>
2017-09-24 12:40:40 +0200

    [0.1] imagemonkey-core: fixed bug in image scaling

--- js/fabric_helper.js
+++ js/fabric_helper.js
@@ -1,26 +1,36 @@
-function setCanvasBackgroundImageUrl(canvas, url) {
-            if (url && url.length > 0) {
-                fabric.Image.fromURL(url, function (img) {
-                    scaleAndPositionImage(canvas, img);
-                });
-            } else {
-                canvas.backgroundImage = 0;
-                canvas.setBackgroundImage('', canvas.renderAll.bind(canvas));
-
-            }
-          }
-
-function scaleAndPositionImage(canvas, img) {
-        var scaleFactor = 0.5;
-
-        canvas.setBackgroundImage(img,
-            canvas.renderAll.bind(canvas), {
-                scaleX: scaleFactor,
-                scaleY: scaleFactor
+function setCanvasBackgroundImageUrl(canvas, url, callback) {
+    if (url && url.length > 0) {
+        fabric.Image.fromURL(url, function (img) {
+            scaleAndPositionImage(canvas, img, callback);
         });
-        canvas.setWidth(img.width * scaleFactor);
-        canvas.setHeight(img.height * scaleFactor);
-        canvas.calcOffset();
+    } else {
+        canvas.backgroundImage = 0;
+        canvas.setBackgroundImage('', canvas.renderAll.bind(canvas));
 
-        canvas.renderAll();
-      }
+    }
+}
+
+function scaleAndPositionImage(canvas, img, callback) {
+    var scaleFactor = calcScaleFactor(img);
+    canvas.setBackgroundImage(img,
+        canvas.renderAll.bind(canvas), {
+            scaleX: scaleFactor,
+            scaleY: scaleFactor
+        }
+    );
+    canvas.setWidth(img.width * scaleFactor);
+    canvas.setHeight(img.height * scaleFactor);
+    canvas.calcOffset();
+
+    canvas.renderAll();
+    typeof callback === 'function' && callback();
+}
+
+function calcScaleFactor(img){
+    var maxImageWidth = 600.0;
+    var scaleFactor = maxImageWidth/img.width;
+    if(scaleFactor > 1.0)
+        scaleFactor = 1.0;
+
+    return scaleFactor;
+}
