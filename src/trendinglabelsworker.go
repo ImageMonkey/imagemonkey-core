a76d1ba77c7eff26b72ba4f49a2d1278dfe1546d
BernhardB <schluchti@gmail.com>
2018-04-27 18:43:43 +0200

    [0.3] imagemonkey-core: add github comment when num of trending labels in use exceed threshold

--- src/trendinglabelsworker.go
+++ src/trendinglabelsworker.go
@@ -10,6 +10,7 @@ import (
 	"flag"
 	"context"
 	"golang.org/x/oauth2"
+	"fmt"
 )
 
 var db *sql.DB
@@ -17,19 +18,23 @@ var db *sql.DB
 type TrendingLabel struct {
 	Name string `json:"name"`
 	Id string `json:"id"`
+	Count int32 `json:"count"`
+
+	GithubIssue struct {
+        Id int `json:"id"`
+        Exists bool `json:"exists"`
+    } `json:"github_issue"`
 }
 
 func getNewTrendingLabels() ([]TrendingLabel, error) {
 	var trendingLabels []TrendingLabel
 
 	trendingLabelTreshold := 20
-	rows, err := db.Query(`SELECT s.id, s.name FROM label_suggestion s 
-						   JOIN image_label_suggestion i ON i.label_suggestion_id = s.id
-						   WHERE s.id NOT IN (
-						   	 SELECT label_suggestion_id FROM trending_label_suggestion
-						   ) 
-						   GROUP BY s.name, s.id
-						   HAVING COUNT(*) > $1`, trendingLabelTreshold) 
+	rows, err := db.Query(`SELECT s.id, s.name, COUNT(t.id), COALESCE(github_issue_id, -1) FROM label_suggestion s 
+							JOIN image_label_suggestion i ON i.label_suggestion_id = s.id
+							LEFT JOIN trending_label_suggestion t ON t.label_suggestion_id = s.id
+							GROUP BY s.name, s.id, num_of_last_sent, github_issue_id
+							HAVING COUNT(*) > (COALESCE(num_of_last_sent, 0) + $1)`, trendingLabelTreshold) 
 	if err != nil {
 		return trendingLabels, err
 	}
@@ -37,9 +42,15 @@ func getNewTrendingLabels() ([]TrendingLabel, error) {
 
 	for rows.Next() {
 		var trendingLabel TrendingLabel 
-		err := rows.Scan(&trendingLabel.Id, &trendingLabel.Name)
+		err := rows.Scan(&trendingLabel.Id, &trendingLabel.Name, &trendingLabel.Count, &trendingLabel.GithubIssue.Id)
 		if err != nil {
 			return trendingLabels, err
+		}
+
+		if trendingLabel.GithubIssue.Id == -1 {
+			trendingLabel.GithubIssue.Exists = false
+		} else {
+			trendingLabel.GithubIssue.Exists = true
 		}
 
 		trendingLabels = append(trendingLabels, trendingLabel)
@@ -48,8 +59,7 @@ func getNewTrendingLabels() ([]TrendingLabel, error) {
 	return trendingLabels, nil
 }
 
-func createGithubTicket(trendingLabel TrendingLabel, repository string) error {
-
+func createGithubTicket(trendingLabel TrendingLabel, repository string) (TrendingLabel, error) {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: GITHUB_API_TOKEN},
@@ -67,14 +77,41 @@ func createGithubTicket(trendingLabel TrendingLabel, repository string) error {
 		Body:    github.String(body),
 	}
 
-	_, _, err := client.Issues.Create(ctx, GITHUB_PROJECT_OWNER, repository, issueRequest)
+	issue, _, err := client.Issues.Create(ctx, GITHUB_PROJECT_OWNER, repository, issueRequest)
+
+	if err == nil {
+		trendingLabel.GithubIssue.Id = *issue.Number
+		trendingLabel.GithubIssue.Exists = true
+	}
+
+	return trendingLabel, err
+}
+
+func addCommentToGithubTicket(trendingLabel TrendingLabel, repository string) error {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: GITHUB_API_TOKEN},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	body := fmt.Sprintf("New label count: %d" ,trendingLabel.Count)
+
+	//create a new comment
+	commentRequest := &github.IssueComment{
+		Body:    github.String(body),
+	}
+
+	_, _, err := client.Issues.CreateComment(ctx, GITHUB_PROJECT_OWNER, repository, trendingLabel.GithubIssue.Id, commentRequest)
 
 	return err
 }
 
-func markTrendingLabelAsSent(trendingLabel TrendingLabel) error {
-	_, err := db.Exec("INSERT INTO trending_label_suggestion(label_suggestion_id, sent) VALUES($1, $2)", 
-						trendingLabel.Id, true)
+func updateSentTrendingLabelCount(trendingLabel TrendingLabel) error {
+	_, err := db.Exec(`INSERT INTO trending_label_suggestion(label_suggestion_id, num_of_last_sent, github_issue_id) VALUES($1, $2, $3)
+						ON CONFLICT(label_suggestion_id) DO UPDATE SET num_of_last_sent = $2, github_issue_id = $3`, 
+						trendingLabel.Id, trendingLabel.Count, trendingLabel.GithubIssue.Id)
 	return err
 }
 
@@ -121,17 +158,32 @@ func main() {
 		} else {
 			for _, trendingLabel := range trendingLabels {
 				log.Info("[Main] Detected a new trending label: ", trendingLabel.Name)
-				log.Info("[Main] Creating Github ticket for trending label: ", trendingLabel.Name)
-				//there is a new trending label...create a github ticket for that
-				err := createGithubTicket(trendingLabel, *repository)
-				if err != nil {
-					log.Error("[Main] Couldn't create github issue for trending label: ", err.Error())
-					raven.CaptureError(err, nil)
-				} else {
-					err := markTrendingLabelAsSent(trendingLabel)
+
+				if !trendingLabel.GithubIssue.Exists {
+					//there is a new trending label...create a github ticket for that
+					log.Info("[Main] Creating Github ticket for trending label: ", trendingLabel.Name)
+					tl, err := createGithubTicket(trendingLabel, *repository)
 					if err != nil {
-						log.Error("[Main] Couldn't mark trending label as sent: ", err.Error())
+						log.Error("[Main] Couldn't create github issue for trending label: ", err.Error())
 						raven.CaptureError(err, nil)
+					} else {
+						err := updateSentTrendingLabelCount(tl)
+						if err != nil {
+							log.Error("[Main] Couldn't mark trending label as sent: ", err.Error())
+							raven.CaptureError(err, nil)
+						}
+					}
+				} else { //ticket exists, just add a comment
+					err = addCommentToGithubTicket(trendingLabel, *repository)
+					if err != nil {
+						log.Error("[Main] Couldn't update trending label count for trending label: ", err.Error())
+						raven.CaptureError(err, nil)
+					} else {
+						err := updateSentTrendingLabelCount(trendingLabel)
+						if err != nil {
+							log.Error("[Main] Couldn't mark trending label as sent: ", err.Error())
+							raven.CaptureError(err, nil)
+						}
 					}
 				}
 			}
