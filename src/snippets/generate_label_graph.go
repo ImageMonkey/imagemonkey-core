4544f4b0104e7e1ac1981dd977331f16e272398e
BernhardB <schluchti@gmail.com>
2018-05-19 15:55:08 +0200

    [0.3] imagemonkey-core: added snippets folder
    * contains some small scripts were created during feature development
      to sketch some ideas. Not all of them are useful, but its a shame to
      throw them away; so we store them here in case we need them someday.
    
    see #118

---
+++ src/snippets/generate_label_graph.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/awalterschulze/gographviz"
+	"github.com/satori/go.uuid"
+	"io/ioutil"
+	"strings"
+	"strconv"
+)
+
+func generateRandomGraph(num int) string {
+	graphAst, _ := gographviz.ParseString(`digraph G {}`)
+	graph := gographviz.NewGraph()
+	if err := gographviz.Analyse(graphAst, graph); err != nil {
+	    panic(err)
+	}
+
+	i := 0
+	prev := ""
+	for i < num {
+		u := uuid.NewV4().String()
+		u = strings.Replace(u, "-", "", -1)
+		u = strconv.Quote(u)
+
+		fmt.Printf("%d", i)
+
+		graph.AddNode("G", u, nil)
+
+		if prev != "" {
+			graph.AddEdge(prev, u, true, nil)
+		}
+
+		prev = u
+
+		i += 1
+	}
+	
+	output := graph.String()
+
+	return output
+}
+
+func main() {
+	fmt.Printf("a")
+
+
+	output := generateRandomGraph(30000)
+	fmt.Printf("writing")
+
+	path := "../wordlists/en/graph_big.dot"
+	err := ioutil.WriteFile(path, []byte(output), 0644)
+	if err != nil {
+		fmt.Printf("%s\n", err.Error())
+		return
+	}
+
+	//fmt.Printf(output)
+}
