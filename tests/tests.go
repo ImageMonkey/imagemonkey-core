8d6d2928ef47ef48afceba12810747915c884093
BernhardB <schluchti@gmail.com>
2018-06-25 21:23:54 +0200

    [0.3] imagemonkey-core: added more integration tests

--- tests/tests.go
+++ tests/tests.go
@@ -10,6 +10,8 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"time"
+	"os/exec"
+	"bytes"
 )
 
 
@@ -25,6 +27,76 @@ func random(min, max int) int {
 func randomBool() bool {
     return rand.Float32() < 0.5
 }
+
+func loadSchema() error {
+	var out, stderr bytes.Buffer
+	schemaPath := "../env/postgres/schema.sql"
+
+	//load schema
+	cmd := exec.Command("psql", "-f", schemaPath, "-d", "imagemonkey", "-U", "postgres")
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+	    fmt.Sprintf("Error executing query. Command Output: %+v\n: %+v, %v", out.String(), stderr.String(), err)
+	    return err
+	}
+
+	return nil
+}
+
+func loadDefaults() error {
+	var out, stderr bytes.Buffer
+	defaultsPath := "../env/postgres/defaults.sql"
+
+	//load defaults
+	cmd := exec.Command("psql", "-f", defaultsPath, "-d", "imagemonkey", "-U", "postgres")
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+	    fmt.Sprintf("Error executing query. Command Output: %+v\n: %+v, %v", out.String(), stderr.String(), err)
+	    return err
+	}
+
+	return nil
+} 
+
+func populateLabels() error {
+	var out, stderr bytes.Buffer
+	cmd := exec.Command("go", "run", "populate_labels.go", "common.go", "api_secrets.go", "--dryrun=false")
+	cmd.Dir = "../src/"
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+	    fmt.Sprintf("Error executing query. Command Output: %+v\n: %+v, %v", out.String(), stderr.String(), err)
+	    return err
+	}
+
+	return nil
+}
+
+func installUuidExtension() error {
+	query := "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\""
+	var out, stderr bytes.Buffer
+
+	//load defaults
+	cmd := exec.Command("psql", "-c", query, "-d", "imagemonkey", "-U", "postgres")
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+	    fmt.Sprintf("Error executing query. Command Output: %+v\n: %+v, %v", out.String(), stderr.String(), err)
+	    return err
+	}
+
+	return nil
+} 
 
 
 type ImageMonkeyDatabase struct {
@@ -52,6 +124,100 @@ func (p *ImageMonkeyDatabase) Open() error {
 	return nil
 }
 
+func (p *ImageMonkeyDatabase) Initialize() error {
+
+	//connect as user postgres, in order to drop + re-create database imagemonkey
+	localDb, err := sql.Open("postgres", "user=postgres sslmode=disable")
+	if err != nil {
+		return err
+	}
+
+	defer localDb.Close()
+
+	//terminate any open database connections (we need to do this, before we can drop the database)
+	_, err = localDb.Exec(`SELECT pg_terminate_backend(pid)
+					  FROM pg_stat_activity
+					  WHERE datname = 'imagemonkey'`)
+	if err != nil {
+		return err
+	}
+
+	_, err = localDb.Exec("DROP DATABASE IF EXISTS imagemonkey")
+	if err != nil {
+		return err
+	}
+
+	_, err = localDb.Exec("CREATE DATABASE imagemonkey OWNER monkey")
+	if err != nil {
+		return err
+	}
+
+	_, err = localDb.Exec("CREATE EXTENSION IF NOT EXISTS temporal_tables")
+	if err != nil {
+		return err
+	}
+
+	/*_, err = localDb.Exec(fmt.Sprintf("CREATE EXTENSION IF NOT EXISTS %s", pq.QuoteIdentifier("uuid-ossp")))
+	if err != nil {
+		return err
+	}*/
+
+	err = installUuidExtension()
+	if err != nil {
+		return err
+	}
+
+	/*_, err = localDb.Exec("GRANT ALL PRIVILEGES ON DATABASE imagemonkey to monkey")
+	if err != nil {
+		return err
+	}
+
+	_, err = localDb.Exec("GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO monkey")
+	if err != nil {
+		return err
+	}
+
+	_, err = localDb.Exec("GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO monkey")
+	*/
+
+	
+	err = loadSchema()
+	if err != nil {
+		return err
+	}
+
+	err = loadDefaults()
+	if err != nil {
+		return err
+	}
+
+	err = populateLabels()
+	if err != nil {
+		return err
+	}
+	
+
+	
+
+
+	return err
+}
+
+func (p *ImageMonkeyDatabase) GiveUserModeratorRights(name string) error {
+	_, err := p.db.Exec("UPDATE account SET is_moderator = true WHERE name = $1", name)
+	if err != nil {
+		return err
+	}
+
+	_, err = p.db.Exec(`INSERT INTO account_permission(account_id, can_remove_label) 
+							SELECT a.id, true FROM account a WHERE a.name = $1`, name)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (p *ImageMonkeyDatabase) GetNumberOfImages() (int32, error) {
 	var numOfImages int32
 	err := p.db.QueryRow("SELECT count(*) FROM image").Scan(&numOfImages)
@@ -60,6 +226,16 @@ func (p *ImageMonkeyDatabase) GetNumberOfImages() (int32, error) {
 	}
 
 	return numOfImages, err
+}
+
+func (p *ImageMonkeyDatabase) GetNumberOfUsers() (int32, error) {
+	var numOfUsers int32
+	err := p.db.QueryRow("SELECT count(*) FROM account").Scan(&numOfUsers)
+	if err != nil {
+		return 0, err
+	}
+
+	return numOfUsers, err
 }
 
 func (p *ImageMonkeyDatabase) GetAllValidationIds() ([]string, error) {
@@ -124,6 +300,10 @@ type ReportResult struct {
     Reason string `json:"reason"`
 }
 
+type LoginResult struct {
+	Token string `json:"token"`
+}
+
 
 type ValidateResult struct {
     Id string `json:"uuid"`
@@ -143,6 +323,49 @@ func testReport(uuid string, reason string) error{
      	SetResult(&ReportResult{}).
      	Post(url)
      return err
+}
+
+func testSignUp(username string, password string, email string) error {
+	numBefore, err := db.GetNumberOfUsers()
+	if err != nil {
+		return err
+	}
+
+
+	url := BASE_URL + API_VERSION + "/signup"
+	resp, err := resty.R().
+    	SetHeader("Content-Type", "application/json").
+     	SetBody(map[string]interface{}{"username": username, "password": password, "email": email}).
+     	Post(url)
+
+    if resp.StatusCode() != 201 {
+		return errors.New("Couldn't signup")
+    }
+
+    numAfter, err := db.GetNumberOfUsers(); 
+    if err != nil {
+		return err
+	}
+
+	if numAfter != numBefore + 1 {
+		return errors.New("number of account entries in database is off by one!")
+	} 
+
+    return err
+}
+
+func testLogin(username string, password string) (string, error) {
+	url := BASE_URL + API_VERSION + "/login"
+	resp, err := resty.R().
+     	SetBasicAuth(username, password).
+     	SetResult(&LoginResult{}).
+     	Post(url)
+
+    if resp.StatusCode() != 200 {
+		return "", errors.New("Couldn't login")
+    }
+
+    return resp.Result().(*LoginResult).Token, err
 }
 
 func testDonate(path string, label string) error {
@@ -199,13 +422,27 @@ func testValidate() error{
 	return err
 }
 
-func testImageValidation(uuid string, param string) error{
+func testImageValidation(uuid string, param string, moderated bool, token string) error{
 	url := BASE_URL + API_VERSION + "/validation/" + uuid + "/validate/" + param
-	resp, err := resty.R().
-			Post(url)
+
+	var resp *resty.Response
+	var err error
+	if moderated {
+		resp, err = resty.R().
+				SetHeader("X-Moderation", "true").
+				SetAuthToken(token).
+				Post(url)
+	} else {
+		resp, err = resty.R().
+				Post(url)
+	}
+
 	if resp.StatusCode() != 200 {
 		return errors.New("Couldn't validate image with uuid " + uuid)
 	}
+
+
+	
 	return err
 }
 
@@ -229,7 +466,7 @@ func testRandomImageValidation(num int) error {
 			return err
 		}
 
-		if err := testImageValidation(randomValidationId, param); err != nil {
+		if err := testImageValidation(randomValidationId, param, false, ""); err != nil {
 			return err
 		}
 
@@ -252,32 +489,125 @@ func testRandomImageValidation(num int) error {
 	return nil
 }
 
+func testRandomModeratedImageValidation(num int, token string) error {
+	for i := 0; i < num; i++ {
+
+
+		randomValidationId, err := db.GetRandomValidationId()
+		if err != nil {
+			return err
+		}
+
+		_, beforeChangeNumInvalid, err := db.GetValidationCount(randomValidationId)
+		if err != nil {
+			return err
+		}
+
+		if err := testImageValidation(randomValidationId, "no", true, token); err != nil {
+			return err
+		}
+
+		_, afterChangeNumInvalid, err := db.GetValidationCount(randomValidationId)
+		if err != nil {
+			return err
+		}
+
+
+		if afterChangeNumInvalid != (beforeChangeNumInvalid + 5) {
+			return errors.New("image validation invalid count is off!")
+		}
+	}
+
+	return nil
+}
+
 func main(){
 	db = NewImageMonkeyDatabase()
 
-	err := db.Open()
+	fmt.Printf("Initializing database...\n")
+	err := db.Initialize()
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Open()
 	if err != nil {
 		log.Fatal("[Main] Couldn't open database: ", err.Error())
 	}
 
 	defer db.Close()
 
+	fmt.Printf("Testing signup...\n")
+	err = testSignUp("testuser", "testpassword", "testuser@imagemonkey.io")
+	if err == nil {
+		fmt.Printf("[SUCCESS] Successfully signed up\n")
+	} else {
+		fmt.Printf("[FAIL] Couldn't signup: %s\n", err.Error())
+		return
+	}
+
+	fmt.Printf("Testing login...\n")
+	_,err = testLogin("testuser", "testpassword")
+	if err == nil {
+		fmt.Printf("[SUCCESS] Successfully logged in\n")
+	} else {
+		fmt.Printf("[FAIL] Login failed: %s\n", err.Error())
+		return
+	}
+
 	fmt.Printf("Testing Image Donation...\n")
 	err = testMultipleDonate()
 	if err == nil {
-		fmt.Printf("Successfully donated images\n")
+		fmt.Printf("[SUCCESS] Successfully donated images\n")
 	} else {
-		fmt.Printf("Donating images FAILED: %s\n", err.Error())
+		fmt.Printf("[FAIL] Couldn't donate images: %s\n", err.Error())
 		return
 	}
 
 	err = testRandomImageValidation(100)
 	if err == nil {
 		fmt.Printf("[SUCCESS] Random Image validation test succeeded\n")
-		return
 	} else {
 		fmt.Printf("[FAIL] Random Image validation test failed %s\n", err.Error())
+		return
 	}
+
+	fmt.Printf("Add user 'moderator'...\n")
+	err = testSignUp("moderator", "moderator", "moderator@imagemonkey.io")
+	if err == nil {
+		fmt.Printf("[SUCCESS] user 'moderator' successfully added\n")
+	} else {
+		fmt.Printf("[FAIL] Couldn't add user 'moderator': %s\n", err.Error())
+		return
+	}
+
+	fmt.Printf("Logging in with user 'moderator'...\n")
+	moderatorToken, err := testLogin("moderator", "moderator")
+	if err == nil {
+		fmt.Printf("[SUCCESS] Successfully logged in user 'moderator'\n")
+	} else {
+		fmt.Printf("[FAIL] Couldn't login user 'moderator': %s\n", err.Error())
+		return
+	}
+
+	fmt.Printf("Giving user 'moderator' moderator permissions...\n")
+	err = db.GiveUserModeratorRights("moderator")
+	if err == nil {
+		fmt.Printf("[SUCCESS] Successfully gave user 'moderator' moderator permissions\n")
+	} else {
+		fmt.Printf("[FAIL] Couldn't give user 'moderator' moderator permissions: %s\n", err.Error())
+		return
+	}
+
+	err = testRandomModeratedImageValidation(100, moderatorToken)
+	if err == nil {
+		fmt.Printf("[SUCCESS] Random moderated Image validation test succeeded\n")
+	} else {
+		fmt.Printf("[FAIL] Random moderated Image validation test failed %s\n", err.Error())
+		return
+	}
+
+
 
 	/*uuid := "48750a63-df1f-48d1-99ee-6c60e535a271"
 	fmt.Printf("Testing Image Reporting...\n")
