2183c3570c14ade9783b23a0e8b60c88515c89f2
Bernhard B <schluchti@gmail.com>
2019-06-28 22:51:25 +0200

    improved docker-compose (for travis testing)

--- env/docker/run_all_tests.sh
+++ env/docker/run_all_tests.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-/tmp/wait-for-it.sh $DB_HOST:$DB_PORT -- echo "Starting integration tests"
+/tmp/wait-for-it.sh $DB_HOST:$DB_PORT -- echo "Database is up"
+while true
+	do
+		db_initialized=$(psql -h localhost -U postgres -lqt | cut -d \| -f 1 | grep "imagemonkey" | xargs)
+		if [[ $db_initialized = "imagemonkey" ]]
+		then
+			echo "Database is initialized"
+			break;
+		else
+			echo "Waiting for the database to be initialized ($db_initialized)"
+		fi
+	done
+
+echo "Starting integration tests (after 5 sec delay)"
+sleep 5
 
 ./test -test.v -test.parallel 1 -donations_dir=/tmp/data/donations/ -unverified_donations_dir=/tmp/data/unverified_donations/ -test.timeout=600m
 retVal=$?
