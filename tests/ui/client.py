b8e017f3eb658123104a497f0b73f81df5ed4105
Bernhard B <schluchti@gmail.com>
2019-08-22 21:39:50 +0200

    fixed bug in UI tests

--- tests/ui/client.py
+++ tests/ui/client.py
@@ -10,224 +10,229 @@ import time
 
 BASE_URL = "http://127.0.0.1:8080"
 
-def check_browser_errors(driver):
-	try:
-		browser_logs = driver.get_log('browser')
-	except (ValueError, WebDriverException) as e:
-		# Some browsers does not support getting logs
-		print("Could not get browser logs for driver %s due to exception: %s", driver, e)
-		return []
 
-	errors = []
-	for entry in browser_logs:
-		if entry['level'] == 'SEVERE':
-			errors.append(entry)
-	return errors
+def check_browser_errors(driver):
+    try:
+        browser_logs = driver.get_log('browser')
+    except (ValueError, WebDriverException) as e:
+        # Some browsers does not support getting logs
+        print("Could not get browser logs for driver %s due to exception: %s", driver, e)
+        return []
+
+    errors = []
+    for entry in browser_logs:
+        if entry['level'] == 'SEVERE':
+            errors.append(entry)
+    return errors
+
 
 def check_for_errors(fn):
-	# The wrapper method which will get called instead of the decorated method:
-	def wrapper(*args, **kwargs):
-		fn(*args, **kwargs) # call the decorated method
-		errors = check_browser_errors(args[0].driver)
-		if len(errors) > 0:
-			raise Exception(errors[0]['message'])
+    # The wrapper method which will get called instead of the decorated method:
+    def wrapper(*args, **kwargs):
+        fn(*args, **kwargs)  # call the decorated method
+        errors = check_browser_errors(args[0].driver)
+        if len(errors) > 0:
+            raise Exception(errors[0]['message'])
 
-	return wrapper  # return the wrapper method
+    return wrapper  # return the wrapper method
 
 
 def _wait_until_cookie_is_set(driver, max_wait):
-	cookie = None
-	while max_wait > 0:
-		cookie = driver.get_cookie("imagemonkey")
-		if cookie is None:
-			time.sleep(0.5)
-			max_wait -= 0.5
-		else:
-			break
+    cookie = None
+    while max_wait > 0:
+        cookie = driver.get_cookie("imagemonkey")
+        if cookie is None:
+            time.sleep(0.5)
+            max_wait -= 0.5
+        else:
+            break
 
-	if cookie is None:
-		raise Exception("Couldn't get ImageMonkey cookie")
-	return cookie
+    if cookie is None:
+        raise Exception("Couldn't get ImageMonkey cookie")
+    return cookie
 
 
 class ImageMonkeyWebClient(object):
-	def __init__(self, driver):
-		self._driver = driver
+    def __init__(self, driver):
+        self._driver = driver
 
-	@property
-	def driver(self):
-		return self._driver
+    @property
+    def driver(self):
+        return self._driver
 
-	@check_for_errors
-	def login(self, username, password, should_succeed):
-		self._driver.get(BASE_URL + "/login")
-		self._driver.find_element_by_id("usernameInput").send_keys(username)
-		self._driver.find_element_by_id ("passwordInput").send_keys(password)
-		self._driver.find_element_by_id("loginButton").click()
+    @check_for_errors
+    def login(self, username, password, should_succeed):
+        self._driver.get(BASE_URL + "/login")
+        self._driver.find_element_by_id("usernameInput").send_keys(username)
+        self._driver.find_element_by_id("passwordInput").send_keys(password)
+        self._driver.find_element_by_id("loginButton").click()
 
-		if should_succeed:
-			cookie = _wait_until_cookie_is_set(self._driver, 2)
-			self._driver.add_cookie(cookie)
-			wait = WebDriverWait(self._driver, 10)
-			wait.until(EC.url_changes(BASE_URL))
+        if should_succeed:
+            cookie = _wait_until_cookie_is_set(self._driver, 2)
+            self._driver.add_cookie(cookie)
+            wait = WebDriverWait(self._driver, 10)
+            wait.until(EC.url_changes(BASE_URL))
 
-	@check_for_errors
-	def signup(self, username, email, password):
-		self._driver.get(BASE_URL + "/signup")
-		self._driver.find_element_by_id("usernameInput").send_keys(username)
-		self._driver.find_element_by_id("passwordInput").send_keys(password)
-		self._driver.find_element_by_id("repeatedPasswordInput").send_keys(password)
-		self._driver.find_element_by_id("emailInput").send_keys(email)
-		self._driver.find_element_by_id("signupButton").click()
+    @check_for_errors
+    def signup(self, username, email, password):
+        self._driver.get(BASE_URL + "/signup")
+        self._driver.find_element_by_id("usernameInput").send_keys(username)
+        self._driver.find_element_by_id("passwordInput").send_keys(password)
+        self._driver.find_element_by_id(
+            "repeatedPasswordInput").send_keys(password)
+        self._driver.find_element_by_id("emailInput").send_keys(email)
+        self._driver.find_element_by_id("signupButton").click()
 
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "signedUpMessage")
-		wait.until(EC.visibility_of_element_located(locator))
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "signedUpMessage")
+        wait.until(EC.visibility_of_element_located(locator))
 
-	@check_for_errors
-	def donate(self, file_path, should_succeed):
-		self._driver.get(BASE_URL + "/donate")
-		time.sleep(0.5)
+    @check_for_errors
+    def donate(self, file_path, should_succeed):
+        self._driver.get(BASE_URL + "/donate")
+        time.sleep(0.5)
 
-		#self._driver.execute_script("$('#labelSelector').dropdown('set selected', '%s');" %(label,))
+        # self._driver.execute_script("$('#labelSelector').dropdown('set selected', '%s');" %(label,))
 
-		elm = self._driver.find_element_by_xpath("//input[@type='file']")
-		elm.send_keys(file_path)
+        elm = self._driver.find_element_by_xpath("//input[@type='file']")
+        elm.send_keys(file_path)
 
-		wait = WebDriverWait(self._driver, 10)
-		if should_succeed:
-			locator = (By.ID, "successMsg")
-			wait.until(EC.visibility_of_element_located(locator))
-		else:
-			locator = (By.ID, "failureMsg")
-			wait.until(EC.visibility_of_element_located(locator))
+        wait = WebDriverWait(self._driver, 10)
+        if should_succeed:
+            locator = (By.ID, "successMsg")
+            wait.until(EC.visibility_of_element_located(locator))
+        else:
+            locator = (By.ID, "failureMsg")
+            wait.until(EC.visibility_of_element_located(locator))
 
+    def unlock_image(self):
+        self._driver.get(BASE_URL + "/image_unlock")
 
-	def unlock_image(self):
-		self._driver.get(BASE_URL + "/image_unlock")
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "yesButton")
+        wait.until(EC.visibility_of_element_located(locator))
 
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "yesButton")
-		wait.until(EC.visibility_of_element_located(locator))
+        self._driver.find_element_by_id("yesButton").click()
 
-		self._driver.find_element_by_id("yesButton").click()
+        wait.until(EC.invisibility_of_element_located(locator))
 
-		wait.until(EC.invisibility_of_element_located(locator))
+    def unlock_multiple_images(self):
+        self._driver.get(BASE_URL + "/image_unlock?mode=browse")
 
-	def unlock_multiple_images(self):
-		self._driver.get(BASE_URL + "/image_unlock?mode=browse")
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "loadingIndicator")
+        wait.until(EC.invisibility_of_element_located(locator))
 
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "loadingIndicator")
-		wait.until(EC.invisibility_of_element_located(locator))
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "imageGrid")
+        wait.until(EC.visibility_of_element_located(locator))
 
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "imageGrid")
-		wait.until(EC.visibility_of_element_located(locator))
+        time.sleep(2) #wait until images are loaded (so that we can select them)
 
-		images = self._driver.find_elements_by_xpath('//div[@id="imageGrid"]/div')
-		for image in images:
-			image.click()
+        images = self._driver.find_elements_by_xpath(
+            '//div[@id="imageGrid"]/div')
+        for image in images:
+            image.click()
 
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "imageUnlockDoneButton")
-		wait.until(EC.visibility_of_element_located(locator))
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "imageUnlockDoneButton")
+        wait.until(EC.visibility_of_element_located(locator))
 
-		self._driver.find_element_by_id("imageUnlockDoneButton").click()
+        self._driver.find_element_by_id("imageUnlockDoneButton").click()
 
-		#after all images unlocked, open unlock page again
-		#and make sure that there are no more images to unlock
-		self._driver.get(BASE_URL + "/image_unlock?mode=browse")
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "warningMessageBoxContent")
-		wait.until(EC.visibility_of_element_located(locator))
+        # after all images unlocked, open unlock page again
+        # and make sure that there are no more images to unlock
+        self._driver.get(BASE_URL + "/image_unlock?mode=browse")
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "warningMessageBoxContent")
+        wait.until(EC.visibility_of_element_located(locator))
 
-		num_of_images_after_unlock = len(self._driver.find_elements_by_xpath('//div[@id="imageGrid"]/div'))
-		assert num_of_images_after_unlock == 0, "there are still images to unlock"
+        num_of_images_after_unlock = len(
+            self._driver.find_elements_by_xpath('//div[@id="imageGrid"]/div'))
+        assert num_of_images_after_unlock == 0, "there are still images to unlock"
 
+    def label_image(self, labels):
+        self._driver.get(BASE_URL + "/label")
 
-	def label_image(self, labels):
-		self._driver.get(BASE_URL + "/label")
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "loadingIndicator")
+        wait.until(EC.invisibility_of_element_located(locator))
 
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "loadingIndicator")
-		wait.until(EC.invisibility_of_element_located(locator))
+        # wait for intro text + dismiss it
+        #locator = (By.CLASS_NAME, "shepherd-button-secondary")
+        # wait.until(EC.visibility_of_element_located(locator))
+        # self._driver.find_element_by_class_name("shepherd-button-secondary").click()
 
-		#wait for intro text + dismiss it
-		#locator = (By.CLASS_NAME, "shepherd-button-secondary")
-		#wait.until(EC.visibility_of_element_located(locator))
-		#self._driver.find_element_by_class_name("shepherd-button-secondary").click()
+        # get all direct children (no grandchildren!) of div with id = 'label'
+        num_of_labels_before = len(
+            self._driver.find_elements_by_xpath('//div[@id="labels"]/div'))
 
-		#get all direct children (no grandchildren!) of div with id = 'label' 
-		num_of_labels_before = len(self._driver.find_elements_by_xpath('//div[@id="labels"]/div'))
+        for label in labels:
+            # Semantic UI hides the <select> and <option> tags and replaces it with nicer looking divs
+            # therefore, selenium won't find the element with find_element_by_id()
+            self._driver.execute_script(("addLabel('%s');" % (label,)))
 
-		for label in labels:
-			#Semantic UI hides the <select> and <option> tags and replaces it with nicer looking divs
-			#therefore, selenium won't find the element with find_element_by_id()
-			self._driver.execute_script(("addLabel('%s');" %(label,)))
+            self._driver.find_element_by_xpath(
+                "//button[contains(text(), 'Add')]").click()
 
-			self._driver.find_element_by_xpath("//button[contains(text(), 'Add')]").click()
+        # get all direct children (no grandchildren!) of div with id = 'label'
+        num_of_labels_after = len(
+            self._driver.find_elements_by_xpath('//div[@id="labels"]/div'))
+        assert (num_of_labels_before + len(labels)
+                ) == num_of_labels_after, "Label Image: num of labels before do not match num of labels after"
 
-		#get all direct children (no grandchildren!) of div with id = 'label' 
-		num_of_labels_after = len(self._driver.find_elements_by_xpath('//div[@id="labels"]/div'))
-		assert (num_of_labels_before + len(labels)) == num_of_labels_after, "Label Image: num of labels before do not match num of labels after"
+        self._driver.find_element_by_id("doneButton").click()
 
-		self._driver.find_element_by_id("doneButton").click()
+    def browse_annotate(self):
+        self._driver.get(BASE_URL + "/annotate?mode=browse")
 
-	def browse_annotate(self):
-		self._driver.get(BASE_URL + "/annotate?mode=browse")
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "browseAnnotationsGoButton")
+        wait.until(EC.visibility_of_element_located(locator))
 
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "browseAnnotationsGoButton")
-		wait.until(EC.visibility_of_element_located(locator))
+        elem = self._driver.find_element_by_id("annotationQuery")
+        elem.send_keys("apple")
 
+        elem = self._driver.find_element_by_id("browseAnnotationsGoButton")
+        elem.click()
 
-		elem = self._driver.find_element_by_id("annotationQuery")
-		elem.send_keys("apple")
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "loadingIndicator")
+        wait.until(EC.invisibility_of_element_located(locator))
 
-		elem = self._driver.find_element_by_id("browseAnnotationsGoButton")
-		elem.click()
+        time.sleep(2) #wait until images are loaded so that we can click them
 
+        images = self._driver.find_elements_by_xpath(
+            '//div[@id="imageGrid"]/div')
+        images[0].click()
 
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "loadingIndicator")
-		wait.until(EC.invisibility_of_element_located(locator))
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "loadingIndicator")
+        wait.until(EC.invisibility_of_element_located(locator))
 
+        canvas = self._driver.find_element_by_id("annotationArea")
+        drawing = ActionChains(self.driver)\
+            .click_and_hold(canvas)\
+            .move_by_offset(-10, -15)\
+            .release()
+        drawing.perform()
 
-		images = self._driver.find_elements_by_xpath('//div[@id="imageGrid"]/div')
-		images[0].click()
+        self._driver.find_element_by_id("doneButton").click()
 
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "loadingIndicator")
-		wait.until(EC.invisibility_of_element_located(locator))
+    """ 
+	Returns the labels from the /label page
+    """
+    def image_labels(self):
+        self._driver.get(BASE_URL + "/label")
 
+        wait = WebDriverWait(self._driver, 10)
+        locator = (By.ID, "loadingIndicator")
+        wait.until(EC.invisibility_of_element_located(locator))
 
-		canvas = self.driver.find_element_by_id("annotationArea")
-		drawing = ActionChains(self.driver)\
-					.click_and_hold(canvas)\
-    				.move_by_offset(-10, -15)\
-    				.release()
-		drawing.perform()
+        # get all direct children (no grandchildren!) of div with id = 'label'
+        labels = self._driver.find_elements_by_xpath('//div[@id="labels"]/div')
+        res = []
+        for label in labels:
+            res.append(label.text)
 
-		self._driver.find_element_by_id("doneButton").click()
-
-
-	""" 
-	    Returns the labels from the /label page
-	"""
-	def image_labels(self):
-		self._driver.get(BASE_URL + "/label")
-
-		wait = WebDriverWait(self._driver, 10)
-		locator = (By.ID, "loadingIndicator")
-		wait.until(EC.invisibility_of_element_located(locator))
-
-		
-		#get all direct children (no grandchildren!) of div with id = 'label' 
-		labels = self._driver.find_elements_by_xpath('//div[@id="labels"]/div')
-		res = []
-		for label in labels:
-			res.append(label.text)
-
-		return res
-
+        return res
--- tests/ui/test_annotate_image.py
+++ tests/ui/test_annotate_image.py
@@ -6,22 +6,25 @@ from webdriver import ImageMonkeyChromeWebDriver
 
 
 class TestAnnotateImage(unittest.TestCase):
-	def setUp(self):
-		self._driver = ImageMonkeyChromeWebDriver()
-		self._client = ImageMonkeyWebClient(self._driver)
+    def setUp(self):
+        self._driver = ImageMonkeyChromeWebDriver()
+        self._client = ImageMonkeyWebClient(self._driver)
 
-	@classmethod
-	def setUpClass(cls):
-		helper.initialize_with_moderator()
+    @classmethod
+    def setUpClass(cls):
+        helper.initialize_with_moderator()
 
-	def tearDown(self):
-		self._driver.quit()
+    def tearDown(self):
+        self._driver.quit()
 
-	def test_annotate_image_should_succeed(self):
-		path = os.path.abspath(".." + os.path.sep + "images" + os.path.sep + "apples" + os.path.sep + "apple1.jpeg")
-		self._client.donate(path, True)
+    def test_annotate_image_should_succeed(self):
+        path = os.path.abspath(".." + os.path.sep + "images" +
+                               os.path.sep + "apples" + os.path.sep + "apple1.jpeg")
+        self._client.donate(path, True)
 
-		self._client.login("moderator", "moderator", True)
-		self._client.unlock_multiple_images()
+        self._client.login("moderator", "moderator", True)
+        self._client.unlock_multiple_images()
 
-		self._client.browse_annotate()
+        self._client.label_image(["apple"])
+
+        self._client.browse_annotate()
